80c20008b223986e84806c64e40fc8fd5ee2dd1f
Wayne Warner <wawjr3d@gmail.com>
2012-04-15 14:59:16 -0400

    refactoring, playing with crazy for loops

--- js/atomic.js
+++ js/atomic.js
@@ -1,42 +1,50 @@
 (function($, Mustache, undefined) {
     
-	var cssId = 1;
+	var cssId = 1,
+		$componentStage = $(".component");
+	
+	function loadTemplate(template, path) {
+		return $.get(path + "/" + template.template);
+	}
+	
+	function loadData(template, path) {
+		return $.get(path + "/" + template.data);
+	}
 	
 	function loadCss(cssFile) {
-		var head = document.getElementsByTagName("HEAD")[0];
-		
-		var css = document.createElement("LINK");
-		css.id = cssId++;
-		css.rel = "stylesheet";
-		css.type = "text/css";
-		css.href = cssFile;
-		
-		head.appendChild(css);
+		$("head").append("<link id='" + cssId++ + "' href='" + cssFile + "' rel='stylesheet' type='text/css' />");
 		
 		return cssId;
 	}
+	
+	function getData(xhr) {
+		return xhr[0];
+	}
+	
+	function loadComponentCSS(cssFiles, path) {
+		for (var i = 0; function() { loadCss(path + "/" + cssFiles[i]); }() || i < cssFiles.length - 1; i++);
+	}
     
-    function loadComponentTemplate(componentPropertiesFile) {
- 
-    	$.get(componentPropertiesFile, function(componentProperties) {
-
-    		var componentPath = componentPropertiesFile.substring(0, componentPropertiesFile.lastIndexOf("/")),
-    			templateXhr = $.get(componentPath + "/" + componentProperties.templates["Default"].template),
-    			dataXhr = $.get(componentPath + "/" + componentProperties.templates["Default"].data);
-    		
-    		loadCss(componentPath + "/" + componentProperties.css[0]);
-    		
-        	$.when(templateXhr, dataXhr)
-        	 .then(function(templateResult, dataResult) {
-        		var template = templateResult[0],
-        			data = dataResult[0],
-        			css = dataResult[0];
-        		
-        		$(".component").html(Mustache.to_html(template, data));
-        	});
+    function loadComponent(propertiesFile) {
+    	
+    	$.get(propertiesFile, function(props) {
+        	var path = propertiesFile.substring(0, propertiesFile.lastIndexOf("/"));
+        	
+        	var finishedloadingTemplate = loadTemplate(props.templates["Default"], path);
+        	var finishedloadingData = loadData(props.templates["Default"], path);
+        	loadComponentCSS(props.css, path);
+        	
+        	$.when(finishedloadingTemplate, finishedloadingData)
+	       	 .then(function(templateResult, dataResult) {
+	       		 
+	       		var template = getData(templateResult),
+	       			data = getData(dataResult);
+	       		
+	       		$componentStage.html(Mustache.to_html(template, data));
+	       	});
     	});
     }
     
-    loadComponentTemplate("components/searchResult/component.json");
+    loadComponent("components/searchResult/component.json");
     
 })(jQuery, Mustache);
