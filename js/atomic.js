eef3b1bb47fe1f4de9d18a5ec54fdbb3cf437347
Wayne Warner <wawjr3d@gmail.com>
2012-04-15 15:45:45 -0400

    refactor component into object for easier manipulation later on

--- js/atomic.js
+++ js/atomic.js
@@ -1,50 +1,78 @@
 (function($, Mustache, undefined) {
-    
+	
 	var cssId = 1,
 		$componentStage = $(".component");
-	
-	function loadTemplate(template, path) {
-		return $.get(path + "/" + template.template);
-	}
-	
-	function loadData(template, path) {
-		return $.get(path + "/" + template.data);
-	}
-	
-	function loadCss(cssFile) {
-		$("head").append("<link id='" + cssId++ + "' href='" + cssFile + "' rel='stylesheet' type='text/css' />");
-		
-		return cssId;
-	}
 	
 	function getData(xhr) {
 		return xhr[0];
 	}
 	
-	function loadComponentCSS(cssFiles, path) {
-		for (var i = 0; function() { loadCss(path + "/" + cssFiles[i]); }() || i < cssFiles.length - 1; i++);
+	var Component = function(propsFile) {
+		this.propsFile = propsFile;
+		this.path = propsFile.substring(0, propsFile.lastIndexOf("/"));
+		this.cssIds = [];
 	}
+	
+	Component.prototype = {
+			
+		loadTemplateFile: function(templateId) {
+			return $.get(this.path + "/" + this.props.templates[templateId].template);
+		},
+		
+		loadDataFile: function(templateId) {
+			return $.get(this.path + "/" + this.props.templates[templateId].data);
+		},
+		
+		loadCss: function(cssFile) {
+			var cssFileId = cssId++;
+			$("head").append("<link data-component-id='" + cssFileId + "' href='" + cssFile + "' rel='stylesheet' type='text/css' />");
+			
+			this.cssIds.push(cssFileId);
+		},
+		
+		loadAllCss: function() {
+			var cssFiles = this.props.css;
+			for (var i = 0; this.loadCss(this.path + "/" + cssFiles[i]) || i < cssFiles.length - 1; i++);
+		},
+		
+		unloadAllCss: function() {
+			for (var i = 0; i < this.cssIds.length; i++) {
+				$("head link[data-component-id='" + this.cssIds[i] + "']").remove();
+			}
+		},
+		
+		loadProps: function() {
+			return this.props || $.get(this.propsFile, $.proxy(function(props) {
+				this.props = props;
+			}, this));
+		},
+			
+		load: function() {
+			$.when(this.loadProps()).then($.proxy(function(props) {
+	        	
+	        	this.loadAllCss();
+				
+	        	var finishedloadingTemplate = this.loadTemplateFile("Default");
+	        	var finishedloadingData = this.loadDataFile("Default");
+	        	
+	        	$.when(finishedloadingTemplate, finishedloadingData)
+		       	 .then(function(templateResult, dataResult) {
+		       		 
+		       		var template = getData(templateResult),
+		       			data = getData(dataResult);
+		       		
+		       		$componentStage.html(Mustache.to_html(template, data));
+		       	});
+	    	}, this));	
+		},
+		
+		destroy: function() {
+			this.unloadAllCss();
+		}
+	};
+
     
-    function loadComponent(propertiesFile) {
-    	
-    	$.get(propertiesFile, function(props) {
-        	var path = propertiesFile.substring(0, propertiesFile.lastIndexOf("/"));
-        	
-        	var finishedloadingTemplate = loadTemplate(props.templates["Default"], path);
-        	var finishedloadingData = loadData(props.templates["Default"], path);
-        	loadComponentCSS(props.css, path);
-        	
-        	$.when(finishedloadingTemplate, finishedloadingData)
-	       	 .then(function(templateResult, dataResult) {
-	       		 
-	       		var template = getData(templateResult),
-	       			data = getData(dataResult);
-	       		
-	       		$componentStage.html(Mustache.to_html(template, data));
-	       	});
-    	});
-    }
-    
-    loadComponent("components/searchResult/component.json");
+    var component = new Component("components/searchResult/component.json");
+    component.load();
     
 })(jQuery, Mustache);
