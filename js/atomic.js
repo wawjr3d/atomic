7d12ac7d227f50f1385e7257aa3cb33016589056
Wayne Warner <wawjr3d@gmail.com>
2012-07-31 01:38:38 -0400

    refactor component out of atomic spaghetti code, use componentList as dataSource for autocomplete
    it was getting too hard to work with the bad code and add new features,
    this refactoring helps get to a place where its easier to add new
    features, this commit removes the ability to display a component, we
    need to add it back in as another object that handles that rather than
    leaving the responsibility with the component
    
    this commit also makes the componentList an array like object which
    should be easier to use in general

--- js/atomic.js
+++ js/atomic.js
@@ -1,235 +1,52 @@
-(function($, Mustache, ComponentList, PatternList, undefined) {
+(function($, Component, ComponentList, PatternList, undefined) {
 
-    String.prototype.escapeHTML = function () {                                                                                                        
-        return this.replace(/>/g,'&gt;')
-                    .replace(/</g,'&lt;')
-                    .replace(/"/g,'&quot;');
-    };
-
-	var $componentView = $("#component-view"),
-	    $componentStage = $componentView.children(".component"),
-	    $componentStyles = $componentView.children("style"),
-	    $componentStates = $("#states"),
-	    $componentDetails = $("#component-details"), 
-	    $componentTitle = $componentDetails.find("h1"),
-	    $componentAuthor = $componentDetails.find(".author"),
-	    $componentData = $("#data"),
-	    $componentHTML = $("#component-copy").find(".html"),
-	    $componentCss = $("#component-copy").find(".css"),
-	    loadedComponents = {};
-
-	function addCopyButton($code, text) {
-        var $copyButton = $("<span class='copy'/>");
-        
-        $code.prepend($copyButton);
-        
-        $copyButton.zclip({
-            path: "zclip/ZeroClipboard.swf",
-            copy: text,
-            afterCopy: function() {
-                console.log("copied");
-            }
-        });
-	}
-	
-	// TODO: make this an object and pull out code that shouldnt be a part of
-	// the object
-	var Component = function(propsFile) {
-		this.propsFile = propsFile;
-		this.path = propsFile.substring(0, propsFile.lastIndexOf("/"));
-
-		// TODO: cache by file name to prevent loading files more than once
-		this.templates = {};
-		this.data = {};
-
-		this.templatesLoaded = $.Deferred();
-		this.dataLoaded = $.Deferred();
-		this.propsLoaded = $.Deferred();
-	}
-
-	Component.prototype = {
-
-		loadTemplateFile : function(templateId) {
-			if (this.templates[templateId]) {
-				return;
-			}
-
-			return $.get(this.path + "/"
-					+ this.props.templates[templateId].template, $.proxy(
-					function(template) {
-						this.templates[templateId] = template;
-					}, this), "html");
-		},
-
-		loadAllTemplates : function() {
-			var templates = [];
-
-			for (templateId in this.props.templates) {
-				if (!this.props.templates.hasOwnProperty(templateId)) {
-					continue;
-				}
-				templates.push(this.loadTemplateFile(templateId));
-			}
-
-			$.when.apply(this, templates).then($.proxy(function() {
-				this.templatesLoaded.resolve();
-			}, this));
-		},
-
-		loadDataFile : function(templateId) {
-			if (this.templates[templateId]) {
-				return;
-			}
-
-			return $.get(this.path + "/"
-					+ this.props.templates[templateId].data, $.proxy(function(
-					data) {
-				this.data[templateId] = data;
-			}, this), "json");
-		},
-
-		loadAllData : function() {
-			var datas = [];
-
-			for (templateId in this.props.templates) {
-				if (!this.props.templates.hasOwnProperty(templateId)) {
-					continue;
-				}
-				datas.push(this.loadDataFile(templateId));
-			}
-
-			$.when.apply(this, datas).then($.proxy(function() {
-				this.dataLoaded.resolve();
-			}, this));
-		},
-
-		loadAllCss : function() {
-			var cssFiles = this.props.css;
-			var cssPaths = $.map(cssFiles, $.proxy(function(cssFile) {
-				return "@import '" + this.path + "/" + cssFile + "';";
-			}, this));
-
-			var css = cssPaths.join("\n");
-			$componentStyles.append(css);
-			$componentCss.html(prettyPrintOne(css, "css", true));
-			addCopyButton($componentCss, css);
-		},
-
-		unloadAllCss : function() {
-			$componentStyles.empty();
-		},
-
-		loadProps : function() {
-			return this.props || $.get(this.propsFile, $.proxy(function(props) {
-				this.props = props;
-				this.propsLoaded.resolve();
-			}, this), "json");
-		},
-
-		load : function() {
-			$.when(this.loadProps()).then($.proxy(function() {
-				this.loadAllCss();
-				this.loadAllTemplates();
-				this.loadAllData();
-			}, this));
-		},
-
-		displayTemplate : function(templateId) {
-			$.when(this.templatesLoaded, this.dataLoaded)
-			 .then($.proxy(function() {
-			     var template = this.templates[templateId],
-			         data = this.data[templateId];
-			     var html = Mustache.to_html(template, data);
-			     $componentStage.html(html);
-			     this.displayCopyHTML(html);
-			 }, this));
-		},
-
-		displayCopyHTML: function(html) {
-		    $componentHTML.html(prettyPrintOne(html.escapeHTML(), "html", true));
-		    addCopyButton($componentHTML, html);
-		},
-		
-		displayDetails : function() {
-			$.when(this.propsLoaded).then($.proxy(function() {
-				$componentTitle.html(this.props.name);
-				$componentAuthor.html(this.props.author);
-			}, this));
-
-			$.when(this.templatesLoaded).then($.proxy(function() {
-				var $templateList = $("<ul/>");
-
-				for (var templateId in this.templates) {
-					if (!this.templates.hasOwnProperty(templateId)) {
-						continue;
-					}
-
-					var $li = $("<li/>").html(templateId).click((function(tid, component) {
-					    return function(e) {
-					        component.displayTemplate(tid);
-					    }
-					})(templateId, this));
-
-					$templateList.append($li);
-				}
-
-				$componentStates.find("ul").replaceWith($templateList);
-			}, this));
-
-			$.when(this.dataLoaded).then($.proxy(function() {
-			    $componentData.find("form").replaceWith(json2form(this.data["Default"]));
-			}, this));
-		},
-
-		destroy : function() {
-			this.unloadAllCss();
-		}
-	};
+    var loadedComponents = {};
 
 	function loadComponent(url) {
 		var component = loadedComponents[url];
 		if (!loadedComponents[url]) {
-			loadedComponents[url] = component = new Component(url);
+			loadedComponents[url] = component;
+			//component.load();
 		}
-
-		component.load();
-		component.displayDetails();
-		component.displayTemplate("Default");
+		
+//		component.displayDetails();
+//		component.displayTemplate("Default");
 	}
-
-	loadComponent("components/searchResult/component.json");
 
 	var components = new ComponentList("resources/components.json");
     var patterns = new PatternsList("resources/patterns.json");
+    
 	// TODO: view events go somewhere else? -ck
 	$("ul.tabs").tabs("div.panes > div", {
 		effect: "fade",
 		tabs : "li"
 	});
 
-	 components.load();
-	 patterns.load();
+	patterns.load();
 
-	$.when(components.loaded, patterns.patternsLoaded)
-	 .then(function() {
-	     $('#search input[type=text]').autocomplete({
-	         dataSource : components.components,
-	         minimumCharacters : 0,
-	         resultsDestination : "#component-list",
-	         doInitialRetrieve : true,
-	         itemValue : function(item) {
-	             return item.name;
-	         },
-	         itemDisplay : function(item) {
-	             return item.name;
-	         },
-	         filter : function(value, query) {
-	             return value.toLowerCase().indexOf(query.toLowerCase()) > -1;
-	         }
-	     })
-	     .bind("autocomplete:item:selected", function(e, item) {
-	         loadComponent(item.url);
-	     }).focus();
+	$.when(components.load(), patterns.patternsLoaded)
+	 .done(function() {
+	     components.loadComponentsProps()
+    	     .done(function() {
+    	         $('#search input[type=text]').autocomplete({
+    	             dataSource : components,
+    	             minimumCharacters : 0,
+    	             resultsDestination : "#component-list",
+    	             doInitialRetrieve : true,
+    	             itemValue : function(item) {
+    	                 return item.getName();
+    	             },
+    	             itemDisplay : function(item) {
+    	                 return item.getName();
+    	             },
+    	             filter : function(value, query) {
+    	                 return value.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    	             }
+    	         })
+    	         .bind("autocomplete:item:selected", function(e, item) {
+    	             item.load();
+    	         }).focus();
+    	     });
 	 });
 
-})(jQuery, Mustache, ComponentList, PatternsList);
+})(jQuery, Component, ComponentList, PatternsList);
--- js/components.js
+++ js/components.js
@@ -1,21 +1,212 @@
-(function(global) {
+(function(global, $, Mustache, undefined) {
     "use strict";
 
-    var ComponentList = function(propsFile) {
-        this.propsFile = propsFile;
-        this.components = {};
-        this.loaded = $.Deferred();
+    String.prototype.escapeHTML = function () {                                                                                                        
+        return this.replace(/>/g,'&gt;')
+                    .replace(/</g,'&lt;')
+                    .replace(/"/g,'&quot;');
+    };
+
+    var $componentView = $("#component-view"),
+        $componentStage = $componentView.children(".component"),
+        $componentStyles = $componentView.children("style"),
+        $componentStates = $("#states"),
+        $componentDetails = $("#component-details"), 
+        $componentTitle = $componentDetails.find("h1"),
+        $componentAuthor = $componentDetails.find(".author"),
+        $componentData = $("#data"),
+        $componentHTML = $("#component-copy").find(".html"),
+        $componentCss = $("#component-copy").find(".css");
+    
+    function addCopyButton($code, text) {
+        var $copyButton = $("<span class='copy'/>");
+        
+        $code.prepend($copyButton);
+        
+        $copyButton.zclip({
+            path: "zclip/ZeroClipboard.swf",
+            copy: text,
+            afterCopy: function() {
+                console.log("copied");
+            }
+        });
+    }
+    
+    var Component = function(descriptor) {
+        this.descriptor = descriptor;
+        this.path = descriptor.substring(0, descriptor.lastIndexOf("/"));
+
+        // TODO: cache by file name to prevent loading files more than once
+        this.templates = {};
+        this.data = {};
+
+        this.loaded = null;
+        this.templatesLoaded = null;
+        this.dataLoaded = null;
+        this.propsLoaded = null;
+    };
+    
+    Component.prototype = {
+
+        loadTemplateFile: function(templateId) {
+            if (this.templates[templateId]) { return; }
+
+            return $.get(this.path + "/" + this.props.templates[templateId].template, $.proxy(function(template) {
+                this.templates[templateId] = template;
+            }, this), "html");
+        },
+
+        loadAllTemplates: function() {
+            var templates = [];
+
+            for (var templateId in this.props.templates) {
+                if (this.props.templates.hasOwnProperty(templateId)) {
+                    templates.push(this.loadTemplateFile(templateId));
+                }
+            }
+
+            return this.templatesLoaded = $.when.apply(this, templates);
+        },
+
+        loadDataFile: function(templateId) {
+            if (this.templates[templateId]) {
+                return;
+            }
+
+            return $.get(this.path + "/" + this.props.templates[templateId].data, $.proxy(function(data) {
+                this.data[templateId] = data;
+            }, this), "json");
+        },
+
+        loadAllData: function() {
+            var datas = [];
+
+            for (var templateId in this.props.templates) {
+                if (this.props.templates.hasOwnProperty(templateId)) {
+                    datas.push(this.loadDataFile(templateId));
+                }
+            }
+
+            return this.dataLoaded = $.when.apply(this, datas);
+        },
+
+        loadAllCss: function() {
+            var cssFiles = this.props.css;
+            var cssPaths = $.map(cssFiles, $.proxy(function(cssFile) {
+                return "@import '" + this.path + "/" + cssFile + "';";
+            }, this));
+
+            var css = cssPaths.join("\n");
+            $componentStyles.append(css);
+            $componentCss.html(prettyPrintOne(css, "css", true));
+            addCopyButton($componentCss, css);
+        },
+
+        unloadAllCss: function() {
+            $componentStyles.empty();
+        },
+
+        loadProps: function() {
+            return this.propsLoaded = $.get(this.descriptor, $.proxy(function(props) {
+                this.props = props;
+            }, this), "json");
+        },
+        
+        load: function() {
+            this.loaded = $.when(this.loadProps()).pipe($.proxy(function() {
+                return $.when.apply(this, [
+                   this.loadAllCss(),
+                   this.loadAllTemplates(),
+                   this.loadAllData()
+               ]);
+            }, this));
+            
+            return this.loaded;
+        },
+        
+        destroy : function() {
+            this.unloadAllCss();
+        },
+        
+        getName: function() {
+            return this.props.name;
+        }
+    };
+    
+    
+    var ComponentList = function(descriptor) {
+        this.descriptor = descriptor;
+        this.components = [];
+        this.loaded = null;
     };
 
     ComponentList.prototype = {
         load: function() {
-            $.getJSON(this.propsFile, $.proxy(function(data) {
+            if (this.loaded) { return this.loaded; }
+            
+            return this.loaded = $.getJSON(this.descriptor, $.proxy(function(data) {
                 this.components = data.components;
-                this.loaded.resolve();
+                for(var i = 0; i < data.components.length; i++) {
+                    var component = data.components[i];
+                    Array.prototype.push.call(this, new Component(component.url));
+                }
             }, this));
+        },
+        
+        loadComponentsProps: function() {
+            var loadingComponentsProps = $.map(this, function(component) {
+                return component.loadProps();
+            });
+            
+            return $.when.apply(this, loadingComponentsProps);
         }
     };
     
     global.ComponentList = ComponentList;
-})(this);
-
+    global.Component = Component;
+})(this, jQuery, Mustache);
+//
+//displayTemplate : function(templateId) {
+//    $.when(this.templatesLoaded, this.dataLoaded)
+//     .then($.proxy(function() {
+//         var template = this.templates[templateId],
+//             data = this.data[templateId];
+//         var html = Mustache.to_html(template, data);
+//         $componentStage.html(html);
+//         this.displayCopyHTML(html);
+//     }, this));
+//},
+//
+//displayCopyHTML: function(html) {
+//    $componentHTML.html(prettyPrintOne(html.escapeHTML(), "html", true));
+//    addCopyButton($componentHTML, html);
+//},
+//
+//displayDetails : function() {
+//    $.when(this.propsLoaded).then($.proxy(function() {
+//        $componentTitle.html(this.props.name);
+//        $componentAuthor.html(this.props.author);
+//    }, this));
+//
+//    $.when(this.templatesLoaded).then($.proxy(function() {
+//        var $templateList = $("<ul/>");
+//
+//        for (var templateId in this.templates) {
+//            if (this.templates.hasOwnProperty(templateId)) {
+//                var $li = $("<li/>").html(templateId).click((function(tid, component) {
+//                    return function(e) {
+//                        component.displayTemplate(tid);
+//                    }
+//                })(templateId, this));
+//
+//                $templateList.append($li);
+//            }
+//        }
+//
+//        $componentStates.find("ul").replaceWith($templateList);
+//    }, this));
+//
+//    $.when(this.dataLoaded).then($.proxy(function() {
+//        $componentData.find("form").replaceWith(json2form(this.data["Default"]));
+//    }, this));
+//},
